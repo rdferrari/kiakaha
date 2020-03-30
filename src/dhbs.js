44fe4ee756a570d31b2ae833724fe797e4b9f357
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2020-03-30 21:00:59 +1300

    Add app

---
+++ src/dhbs.js
@@ -0,0 +1,102 @@
+export const dhbs = [
+  {
+    name: "Northland",
+    long: 173.885955,
+    lat: -35.589172
+  },
+  {
+    name: "Waitematā",
+    long: 174.51805,
+    lat: -36.743409
+  },
+  {
+    name: "Auckland",
+    long: 174.76519,
+    lat: -36.856777
+  },
+  {
+    name: "Counties Manukau",
+    long: 174.787712,
+    lat: -37.170012
+  },
+  {
+    name: "Waikato",
+    long: 175.204661,
+    lat: -37.96715
+  },
+  {
+    name: "Bay of Plenty",
+    long: 176.756532,
+    lat: -37.955299
+  },
+  {
+    name: "Taranaki",
+    long: 173.88,
+    lat: -39.25195
+  },
+  {
+    name: "Lakes",
+    long: 176.143993,
+    lat: -38.621427
+  },
+  {
+    name: "Tairawhiti",
+    long: 178.161382,
+    lat: -38.433486
+  },
+  {
+    name: "Whanganui",
+    long: 175.370812,
+    lat: -39.933648
+  },
+  {
+    name: "MidCentral",
+    long: 176.385836,
+    lat: -40.544195
+  },
+  {
+    name: "Hawke's Bay",
+    long: 176.757484,
+    lat: -39.353227
+  },
+  {
+    name: "Capital and Coast",
+    long: 174.744351,
+    lat: -41.268685
+  },
+  {
+    name: "Hutt Valley",
+    long: 175.06405,
+    lat: -41.145464
+  },
+  {
+    name: "Wairarapa",
+    long: 175.505533,
+    lat: -41.379245
+  },
+  {
+    name: "Nelson Marlborough",
+    long: 173.151442,
+    lat: -41.593424
+  },
+  {
+    name: "West Coast",
+    long: 170.570719,
+    lat: -43.142813
+  },
+  {
+    name: "Canterbury",
+    long: 172.031982,
+    lat: -43.610815
+  },
+  {
+    name: "South Canterbury",
+    long: 170.839624,
+    lat: -44.320777
+  },
+  {
+    name: "Southern",
+    long: 169.039601,
+    lat: -45.792962
+  }
+];
